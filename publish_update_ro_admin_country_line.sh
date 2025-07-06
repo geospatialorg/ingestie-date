14786a8d03ef8fb36aad95afe244c927bba74d20
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2025-07-06 20:08:35 +0300

    Update publish_update_ro_admin_country_line.sh

--- publish_update_ro_admin_country_line.sh
+++ publish_update_ro_admin_country_line.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 ##############################################################################################################
-#🛠 Publicare/actualizare frontieră liniezz
+#🛠 Publicare/actualizare Județe, România (linie)
 ##############################################################################################################
 
 #🎛 configurații
@@ -11,8 +11,10 @@ source /home/ubuntu/anaconda3/etc/profile.d/conda.sh
 conda activate geo
 
 #🚏 definire căi date
+ins_data_path="/storage/volumes/geoserver-1-storage/brute/institutii_romania/ins/siruta"
 ancpi_data_path="/storage/volumes/geoserver-1-storage/brute/institutii_romania/ancpi/limite_administrative"
-out_data_path="/storage/volumes/geoserver-1-storage/administrative_boundaries/country"
+county_data_path="/storage/volumes/geoserver-1-storage/administrative_boundaries/county"
+tmp_data_path="/storage/volumes/geoserver-1-storage/tmp"
 
 #⚙️ PostGIS
 pg_host="localhost"
@@ -28,75 +30,96 @@ gs_user="user"
 gs_pass="pass"
 gs_workspace="administrative-boundaries"
 gs_store="administrative-boundaries"
-gs_layer_title="Limite administrative - frontiera de stat, România (linie)"
-gs_layer_abstract="Acest set de date conține limitele oficiale ale teritoriului României, în format vectorial de tip linie, procesat de comunitatea geo-spatial.org pe baza datelor publice furnizate de Agenția Națională de Cadastru și Publicitate Imobiliară."
-gs_layer_keywords=("România" "frontiera de stat" "limite administrative" "vector" "linie")
-gs_layer_metadata_link="https://services.geo-spatial.org/geonetwork/srv/eng/catalog.search#/metadata/a824c22d-6243-410e-a89a-d7d11199c077"
-gs_layer_style="ro_admin_country_line"
+gs_layer_title="Limite administrative - județe, România (linie)"
+gs_layer_abstract="Set de date ce conține limitele oficiale ale județelor din România, în format vectorial de tip linie, procesat de comunitatea geo-spatial.org pe baza datelor publice furnizate de Agenția Națională de Cadastru și Publicitate Imobiliară. Versiunea curentă reflectă organizarea administrativ-teritorială valabilă în România anulului 2025."
+gs_layer_keywords=("România" "județe" "limite administrative" "vector" "linie")
+gs_layer_metadata_link="https://services.geo-spatial.org/geonetwork/srv/eng/catalog.search#/metadata/9fa6e7ae-460b-47c0-bae8-bc81b7446386   "
+gs_layer_style="administrative-boundaries:ro_admin_county_line"
 
 #⚙️ Date
-layer_name="ro_admin_country_line"
+layer_name="ro_admin_county_line"
 
 echo "
-🛠 Procesare frontieră linie
-"
+🛠 Procesare Județe, România (linie)
+ "
 
-#💾 creare versiune GPKG
-echo "💾 creare versiune GPKG"
-if [ -f ${out_data_path}/${layer_name}.gpkg ]; then
-    rm ${out_data_path}/${layer_name}.gpkg
-fi
-ogr2ogr -of GPKG -lco FID=id -a_srs "EPSG:3844" -nln ${layer_name} -dialect sqlite -sql "SELECT a.localId AS border, a.beginVers AS version, a.Geometry AS geometry FROM Limita_administrativa_tara AS a" ${out_data_path}/${layer_name}.gpkg ${ancpi_data_path}/Limita_administrativa_tara.shp
+echo "💾 Încărcare date în SQLite"
 
-#💾 creare versiune Shapefile
-echo "💾 creare versiune Shapefile"
-if [ -f ${out_data_path}/${layer_name}.zip ]; then
-    rm ${out_data_path}/${layer_name}.zip
+#💾 încărcare SHP ANCPI în SQLite (se încarcă și datele de tip poligon pentru join-ul ulterior)
+ogr2ogr -of "SQLite" -a_srs "EPSG:3844" -lco LAUNDER=NO ${tmp_data_path}/judete.db ${ancpi_data_path}/Unitate_administrativa_judet.shp
+ogr2ogr -of SQLite -append -a_srs "EPSG:3844" ${tmp_data_path}/judete.db ${ancpi_data_path}/Limita_administrativa_judet.shp
+
+#💾 încărcare tabel regiuni de dezvoltare în SQLite
+csvsql --db sqlite:///${tmp_data_path}/judete.db --insert ${ins_data_path}/siruta_zone.csv
+
+#💾 încărcare tabel județe în SQLITE
+csvsql --db sqlite:///${tmp_data_path}/judete.db --insert ${ins_data_path}/siruta_judete.csv
+
+#💾 creare tabel SQLite cu extragerea codurilor SIRUTA ale județelor vecine unei limite din câmpul localid și salvarea lor în câmpurile leftId și rightId
+sqlite3 ${tmp_data_path}/judete.db "CREATE TABLE split_siruta AS SELECT id, substr(localid, 1, instr(localid, '.') - 1) AS leftId, substr(localid, instr(localid, '.') + 1) AS rightId FROM Limita_administrativa_judet"
+
+#💾 creare tabel pentru stratul de tip poligon (join cu SIRUTA)
+ogr2ogr -of "SQLite" -append -dsco SPATIALITE=YES -lco LAUNDER=NO -a_srs EPSG:3844 -nln ro_admin_county_polygon -sql "SELECT a.OGC_FID AS id, CAST(b.jud AS INTEGER) AS countyId, CAST(b.siruta AS INTEGER) AS countyCode, b.denj AS name, b.mnemonic, CAST(c.zona AS INTEGER) AS regionId, CAST(c.siruta AS INTEGER)  AS regionCode, c.denzona AS region, CAST(b.FSJ AS INTEGER) AS sortCode, a.beginvers AS version, a.GEOMETRY FROM Unitate_administrativa_judet AS a LEFT JOIN siruta_judete AS b ON (a.natcode = b.siruta) LEFT JOIN siruta_zone AS c ON (b.zona=c.zona)" ${tmp_data_path}/judete.db ${tmp_data_path}/judete.db
+
+#💾 creare tabel pentru stratul de tip linie (join cu SIRUTA și stratul poligon)
+ogr2ogr -of "SQLite" -append -dsco SPATIALITE=YES -lco LAUNDER=NO -a_srs EPSG:3844 -nln ro_admin_county_line -sql "SELECT a.OGC_FID AS id, a.id, c.name AS leftCounty, d.name AS rightCounty, CAST(b.leftId AS INTEGER) AS leftId, CAST(b.rightId AS INTEGER) AS rightId, a.beginvers AS version, a.GEOMETRY FROM Limita_administrativa_judet AS a LEFT JOIN split_siruta AS b ON (a.id = b.id) LEFT JOIN ro_admin_county_polygon AS c ON (b.leftId=c.countycode) LEFT JOIN ro_admin_county_polygon AS d ON (b.rightId=d.countycode)" ${tmp_data_path}/judete.db ${tmp_data_path}/judete.db
+
+#💾 creare versiune GeoPackage
+echo creare versiune GeoPackage
+if [ -f ${county_data_path}/ro_admin_county_line.gpkg ]; then
+    rm ${county_data_path}/ro_admin_county_line.gpkg
 fi
-ogr2ogr -lco ENCODING=UTF-8 -nln ${layer_name} -dialect sqlite -sql "SELECT a.id AS id, * FROM ${layer_name} AS a" ${out_data_path}/${layer_name}.shp ${out_data_path}/${layer_name}.gpkg
+ogr2ogr -of GPKG -lco FID=id -a_srs EPSG:3844 -nlt MULTILINESTRING -nln ro_admin_county_line -dialect sqlite -sql "SELECT GEOMETRY AS geometry, * FROM ro_admin_county_line" ${county_data_path}/ro_admin_county_line.gpkg ${tmp_data_path}/judete.db
+
+#💾 creare fișiere Esri Shapefile
+echo "💾 creare fișiere Esri Shapefile"
+if [ -f ${county_data_path}/ro_admin_county_line.zip ]; then
+    rm ${county_data_path}/ro_admin_county_line.zip
+fi
+ogr2ogr -lco ENCODING=UTF-8 -nln ro_admin_county_line -dialect sqlite -sql "SELECT a.id AS id, * FROM ro_admin_county_line AS a" ${county_data_path}/ro_admin_county_line.shp ${county_data_path}/ro_admin_county_line.gpkg
 
 #📦 arhivare fișiere shp
 echo "📦 arhivare fișiere shp"
-zip -j ${out_data_path}/${layer_name}.zip ${out_data_path}/${layer_name}.dbf ${out_data_path}/${layer_name}.shp ${out_data_path}/${layer_name}.prj ${out_data_path}/${layer_name}.shx ${out_data_path}/${layer_name}.cpg
+zip -j ${county_data_path}/ro_admin_county_line.zip ${county_data_path}/ro_admin_county_line.dbf ${county_data_path}/ro_admin_county_line.shp ${county_data_path}/ro_admin_county_line.prj ${county_data_path}/ro_admin_county_line.shx ${county_data_path}/ro_admin_county_line.cpg
 
 #💾 creare versiune FlatGeobuf
 echo "💾 creare versiune FlatGeobuf"
-if [ -f ${out_data_path}/${layer_name}.fgb ]; then
-    rm ${out_data_path}/${layer_name}.fgb
+if [ -f ${county_data_path}/ro_admin_county_line.fgb ]; then
+    rm ${county_data_path}/ro_admin_county_line.fgb
 fi
-ogr2ogr -of FlatGeobuf -nln ${layer_name} -dialect sqlite -sql "SELECT a.id AS id, * FROM ${layer_name} AS a" -nlt MULTILINESTRING ${out_data_path}/${layer_name}.fgb ${out_data_path}/${layer_name}.gpkg
+ogr2ogr -of FlatGeobuf -nlt MULTILINESTRING -nln ro_admin_county_line -dialect sqlite -sql "SELECT a.id AS id, * FROM ro_admin_county_line AS a" ${county_data_path}/ro_admin_county_line.fgb ${county_data_path}/ro_admin_county_line.gpkg
 
 #💾 creare versiune GeoParquet
 echo "💾 creare versiune GeoParquet"
-if [ -f ${out_data_path}/${layer_name}.parquet ]; then
-    rm ${out_data_path}/${layer_name}.parquet
+if [ -f ${county_data_path}/ro_admin_county_line.parquet ]; then
+    rm ${county_data_path}/ro_admin_county_line.parquet
 fi
-ogr2ogr -of Parquet -nlt MULTILINESTRING -nln ${layer_name} -dialect sqlite -sql "SELECT a.id AS id, * FROM ${layer_name} AS a" ${out_data_path}/${layer_name}.parquet ${out_data_path}/${layer_name}.gpkg
+ogr2ogr -of Parquet -nlt MULTILINESTRING -nln ro_admin_county_line -dialect sqlite -sql "SELECT a.id AS id, * FROM ro_admin_county_line AS a" ${county_data_path}/ro_admin_county_line.parquet ${county_data_path}/ro_admin_county_line.gpkg
 
 #💾 creare versiune GeoJSON
 echo "💾 creare versiune GeoJSON"
-if [ -f ${out_data_path}/${layer_name}.geojson ]; then
-    rm ${out_data_path}/${layer_name}.geojson
+if [ -f ${county_data_path}/ro_admin_county_line.geojson ]; then
+    rm ${county_data_path}/ro_admin_county_line.geojson
 fi
-ogr2ogr -of GeoJSON -t_srs EPSG:4326 -nln ${layer_name} -dialect sqlite -sql "SELECT a.id AS id, * FROM ${layer_name} AS a" ${out_data_path}/${layer_name}.geojson ${out_data_path}/${layer_name}.gpkg
+ogr2ogr -of GeoJSON -t_srs EPSG:4326 -nln ro_admin_county_line -dialect sqlite -sql "SELECT a.id AS id, * FROM ro_admin_county_line AS a" ${county_data_path}/ro_admin_county_line.geojson ${county_data_path}/ro_admin_county_line.gpkg
 
 #💾 creare versiune KML
 echo "💾 creare versiune KML"
-if [ -f ${out_data_path}/${layer_name}.kml ]; then
-    rm ${out_data_path}/${layer_name}.kml
+if [ -f ${county_data_path}/ro_admin_county_line.kml ]; then
+    rm ${county_data_path}/ro_admin_county_line.kml
 fi
-ogr2ogr -of KML -t_srs EPSG:4326 -dsco NameField=name ${out_data_path}/${layer_name}.kml ${out_data_path}/${layer_name}.gpkg
+ogr2ogr -of KML -t_srs EPSG:4326 -dsco NameField=name ${county_data_path}/ro_admin_county_line.kml ${county_data_path}/ro_admin_county_line.gpkg
 
 #💾 creare versiune TopoJSON
 echo "💾 creare versiune TopoJSON"
-if [ -f ${out_data_path}/${layer_name}.topojson ]; then
-    rm ${out_data_path}/${layer_name}.topojson
+if [ -f ${county_data_path}/ro_admin_county_line.topojson ]; then
+    rm ${county_data_path}/ro_admin_county_line.topojson
 fi
-mapshaper -i ${out_data_path}/${layer_name}.geojson -o format=topojson ${out_data_path}/${layer_name}.topojson
+mapshaper -i ${county_data_path}/ro_admin_county_line.shp -o format=topojson ${county_data_path}/ro_admin_county_line.topojson
 
 #💾 actualizarea setului de date în baza de date PostGIS
 echo "💾 actualizarea setului de date în baza de date PostGIS"
-ogr2ogr -of PostgreSQL PG:"host=${pg_host} port=${pg_port} user=${pg_user} dbname=${pg_db} password=${pg_pass}" -lco schema=${pg_schema} -lco GEOMETRY_NAME=geom -lco overwrite=yes ${out_data_path}/${layer_name}.gpkg ${layer_name} -skipfailures -overwrite
+ogr2ogr -of PostgreSQL PG:"host=${pg_host} port=${pg_port} user=${pg_user} dbname=${pg_db} password=${pg_pass}" -lco schema=${pg_schema} -lco GEOMETRY_NAME=geom -lco overwrite=yes ${county_data_path}/${layer_name}.gpkg ${layer_name} -skipfailures -overwrite
 
 #🖇 indexare date PostGIS
 psql -h ${pg_host} -p ${pg_port} -U ${pg_user} -d ${pg_db} -c "
@@ -153,12 +176,12 @@ curl -s -u $gs_user:$gs_pass -XPUT -H "Content-type: text/xml" \
     </featureType>" \
 "${gs_url}/rest/workspaces/${gs_workspace}/datastores/${gs_store}/featuretypes/${layer_name}"
 
-#💾 Setare stil implicit
-echo "🎨 Setare stil ${gs_layer_style}..."
+#💾 Setare stil implicit + atașare stil suplimentar
+echo "🎨 Setare stil implicit + atașare stil suplimentar..."
 curl -s -u $gs_user:$gs_pass -XPUT -H "Content-type: text/xml" \
   -d "<layer>
          <defaultStyle>
-             <name>${gs_layer_style}</name>
+             <name>${gs_layer_style##*:}</name>
          </defaultStyle>
      </layer>" \
   "${gs_url}/rest/layers/${gs_workspace}:${layer_name}"
@@ -167,4 +190,4 @@ echo "✅ Stratul ${layer_name} a fost adăugat și configurat cu succes în Geo
 
 #🗑️ Ștergere fișiere intermediare
 echo "🗑️ Ștergere fișiere Shapefile"
-rm ${out_data_path}/${layer_name}.dbf ${out_data_path}/${layer_name}.shp ${out_data_path}/${layer_name}.prj ${out_data_path}/${layer_name}.shx ${out_data_path}/${layer_name}.cpg
+rm ${county_data_path}/${layer_name}.dbf ${county_data_path}/${layer_name}.shp ${county_data_path}/${layer_name}.prj ${county_data_path}/${layer_name}.shx ${county_data_path}/${layer_name}.cpg ${tmp_data_path}/judete.db
